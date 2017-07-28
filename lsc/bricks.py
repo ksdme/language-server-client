c221ad219240394b4458cdb0d55de47341bfa2b1
Kilari Teja <kilariteja9@gmail.com>
2017-07-29 02:03:17 +0530

    Refactor [Broken]

--- lsc/bricks.py
+++ lsc/bricks.py
@@ -1,205 +1,210 @@
-class Brick(object):
-	""" "null" """
-	pass
+"""
+    @author ksdme
+    Contains wrappers for protocol objects
+"""
 
-# ------------------------------------- 
+class Brick(object):
+    """ "null" """
+    pass
+
+# -------------------------------------
 # Document Identification Bricks
 # -------------------------------------
 # Refer to protocol.md#uri
 class DocumentUri(Brick):
-	"""
-		"str()"
-	"""
+    """
+        "str()"
+    """
 
 class TextDocumentIdentifier(Brick):
-	"""
-		{
-			"uri": "class(DocumentUri)"
-		}
-	"""
+    """
+        {
+            "uri": "class(DocumentUri)"
+        }
+    """
 
 # extends TextDocumentIdentifier
 class VersionedTextDocumentIdentifier(Brick):
-	"""
-		{
-			"uri": "class(DocumentUri)",
-			"version": "int()"
-		}
-	"""
+    """
+        {
+            "uri": "class(DocumentUri)",
+            "version": "int()"
+        }
+    """
 
-# ------------------------------------- 
+# -------------------------------------
 # Error/Diagonstic Codes Handlers
 # -------------------------------------
 # protocol.md#diagnostic
 class DiagnosticSeverity(Brick):
-	"""
-		"int_range(1, 4)"
-	"""
+    """
+        "int_range(1, 4)"
+    """
 
 # protocol.md#response-message
 class ErrorCodes(Brick):
-	"""
-		"or(eq(-32700),eq(-32600),eq(-32601),eq(-32602),eq(-32603),eq(-32099),eq(-32000),eq(-32002),eq(-32001),eq(-32800))"
-	"""
+    """
+        "or(eq(-32700),eq(-32600),eq(-32601),eq(-32602),eq(-32603),eq(-32099),eq(-32000),eq(-32002),eq(-32001),eq(-32800))"
+    """
 
 class ResponseError(Brick):
-	"""
-		{
-			"code": 	"int()",
-			"message": 	"str()",
-			"data": 	"opt(class(ErrorCodes))"
-		}
-	"""
+    """
+        {
+            "code":     "int()",
+            "message":  "str()",
+            "data":     "opt(class(ErrorCodes))"
+        }
+    """
 
 # ------------------------------------- 
 # Basic Bricks
 # -------------------------------------
 class Message(Brick):
-	"""
-		{
-			"jsonrpc": "constant(str('2.0'))"
-		}
-	"""
+    """
+        {
+            "jsonrpc": "constant(str('2.0'))"
+        }
+    """
 
 # extends Message
 class RequestMessage(Brick):
-	"""
-		{
-			"jsonrpc": 	"constant(str('2.0'))",
+    """
+        {
+            "jsonrpc":  "constant(str('2.0'))",
 
-			"id": 		"or(int(), str())",
-			"method": 	"str()",
-			"params": 	"opt(any())"
-		}
-	"""
+            "id":       "or(int(), str())",
+            "method":   "str()",
+            "params":   "opt(any())"
+        }
+    """
 
 # extends Message
 class ResponseMessage(Brick):
-	"""
-		{
-			"jsonrpc": "constant(str('2.0'))",
+    """
+        {
+            "jsonrpc": "constant(str('2.0'))",
 
-			"id": 		"or(int(), str(), null())",
-			"result": 	"opt(any())",
-			"error": 	"opt(class(ResponseError))"
-		}
-	"""
+            "id":       "or(int(), str(), null())",
+            "result":   "opt(any())",
+            "error":    "opt(class(ResponseError))"
+        }
+    """
 
 # extends Message
 class NotificationMessage(Brick):
-	"""
-		{
-			"method": "str()",
-			"params": "opt(any())"
-		}
-	"""
+    """
+        {
+            "method": "str()",
+            "params": "opt(any())"
+        }
+    """
 
 class CancelParams(Brick):
-	"""
-		{
-			"id": "or(int(), str())"
-		}
-	"""
+    """
+        {
+            "id": "or(int(), str())"
+        }
+    """
 
-# ------------------------------------- 
+# -------------------------------------
 # Bricks
 # -------------------------------------
 class Position(Brick):
-	"""
-		{
-			"line": 	"pos(int())",
-			"character":"pos(int())"
-		}
-	"""
+    """
+        {
+            "line":     "pos(int())",
+            "character":"pos(int())"
+        }
+    """
 
 class Range(Brick):
-	"""
-		{
-			"start": 	"class(Position)",
-			"end": 		"class(Position)"
-		}
-	"""
+    """
+        {
+            "start":    "class(Position)",
+            "end":      "class(Position)"
+        }
+    """
 
 class Location(Brick):
-	"""
-		{
-			"uri": 	 "class(DocumentUri)",
-			"range": "class(Range)"
-		}
-	"""
+    """
+        {
+            "uri":   "class(DocumentUri)",
+            "range": "class(Range)"
+        }
+    """
 
 class Diagnostic(Brick):
-	"""
-		{
-			"range": 	"class(Range)",
-			"severity": "opt(class(DiagnosticSeverity))",
-			"code": 	"opt(or(int(), str()))",
-			"source": 	"opt(str())",
-			"message": 	"str()"
-		}
-	"""
+    """
+        {
+            "range":    "class(Range)",
+            "severity": "opt(class(DiagnosticSeverity))",
+            "code":     "opt(or(int(), str()))",
+            "source":   "opt(str())",
+            "message":  "str()"
+        }
+    """
 
 class Command(Brick):
-	"""
-		{
-			"title": 		"str()",
-			"command": 		"str()",
-			"arguments": 	"opt(array())"
-		}
-	"""
+    """
+        {
+            "title":        "str()",
+            "command":      "str()",
+            "arguments":    "opt(array())"
+        }
+    """
 
 class TextEdit(Brick):
-	"""
-		{
-			"range": "class(Range)",
-			"newText": "str()"
-		}
-	"""
+    """
+        {
+            "range": "class(Range)",
+            "newText": "str()"
+        }
+    """
 
 class TextDocumentEdit(Brick):
-	"""
-		{
-			"textDocument": "class(VersionedTextDocumentIdentifier)",
-			"edits": 		"array(class(TextEdit))"
-		}
-	"""
+    """
+        {
+            "textDocument": "class(VersionedTextDocumentIdentifier)",
+            "edits":        "array(class(TextEdit))"
+        }
+    """
 
 class WorkspaceEdit(Brick):
-	"""
-		{
-			"changes": "opt(dict(class(DocumentUri), array(class(TextEdit))))",
-			"documentChanges": "array(class(TextDocumentEdit))"
-		}
-	"""
+    """
+        {
+            "changes": "opt(dict(class(DocumentUri), array(class(TextEdit))))",
+            "documentChanges": "array(class(TextDocumentEdit))"
+        }
+    """
 
 class TextDocumentItem(Brick):
-	"""
-		{
-			"uri": 			"class(DocumentUri)",
-			"languageId": 	"str()",
-			"version": 		"int()",
-			"text": 		"str()"
-		}
-	"""
+    """
+        {
+            "uri":          "class(DocumentUri)",
+            "languageId":   "str()",
+            "version":      "int()",
+            "text":         "str()"
+        }
+    """
 
 class TextDocumentPositionParams(Brick):
-	"""
-		{
-			"textDocument": "class(TextDocumentIdentifier)",
-			"position": 	"class(Position)"
-		}
-	"""
+    """
+        {
+            "textDocument": "class(TextDocumentIdentifier)",
+            "position":     "class(Position)"
+        }
+    """
 
 class DocumentFilter(Brick):
-	"""
-		{
-			"language": "opt(str())",
-			"scheme": 	"opt(str())",
-			"pattern": 	"opt(str())"
-		}
-	"""
+    """
+        {
+            "language": "opt(str())",
+            "scheme":   "opt(str())",
+            "pattern":  "opt(str())"
+        }
+    """
 
 class DocumentSelector(Brick):
-	"""
-		"array(class(DocumentFilter))"
-	"""
+    """
+        "array(class(DocumentFilter))"
+    """
--- tests/unit/bricks.py
+++ tests/unit/bricks.py
@@ -1,18 +1,31 @@
-import unittest, json
+"""
+    @author ksdme
+    Tests lsc.bricks module, both in terms of
+    of the raw definitions of the Bricks and
+    also their compiled forms
+"""
+
+import json
+import unittest
+
 from lsc import bricks
 
 class TestUnits(unittest.TestCase):
+    """ TestUnit """
 
-	def test_protocol_units(self):
+    def test_protocol_units(self):
+        """
+            Tests all Brick classes provided
+            by lsc.bricks if their doc strings
+            are json loadbale
+        """
 
-		for l in dir(bricks):
-			avail = getattr(bricks, l)
-			if type(avail) == type:
-				if issubclass(avail, bricks.Brick):
-					try:
-						json.loads(avail.__doc__)
-					except Exception as e:
-						self.assertTrue(False,
-							"{}; {}".format(l, str(e)))
-
-		self.assertTrue(True)
+        for attrbute in dir(bricks):
+            avail = getattr(bricks, attrbute)
+            if isinstance(avail, type):
+                if issubclass(avail, bricks.Brick):
+                    try:
+                        json.loads(avail.__doc__)
+                    except Exception as err:
+                        self.assertTrue(False,
+                                        "{}; {}".format(attrbute, str(err)))

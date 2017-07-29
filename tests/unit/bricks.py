1a7b2517808aab04a91ed895528bfde6e5ec41d5
Kilari Teja <kilariteja9@gmail.com>
2017-07-30 03:45:31 +0530

    Added Unit Tests to Bricks Module

--- tests/unit/bricks.py
+++ tests/unit/bricks.py
@@ -5,10 +5,13 @@
     also their compiled forms
 """
 
-import json
 import unittest
-
 from lsc import bricks
+
+class Messages:
+    """ Messages """
+
+    STRUCT_MISSING = "Missing Structure Descriptor"
 
 class TestUnits(unittest.TestCase):
     """ Basic TestUnit """
@@ -16,5 +19,14 @@ class TestUnits(unittest.TestCase):
     def test_protocol_raw_bricks(self):
         """
             Tests if each brick class has an associated
-            STRUCT
+            STRUCT. Its basic consistency test.
         """
+
+        for elem in dir(bricks):
+            prop = getattr(bricks, elem)
+            if isinstance(prop, type):
+                if issubclass(prop, bricks.Brick):
+                    if not hasattr(prop, "STRUCT"):
+                        if prop is not bricks.Brick:
+                            message = "{}; {}".format(elem, Messages.STRUCT_MISSING)
+                            self.assertTrue(False, message)
